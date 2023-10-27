e873ddf92f1f8eb307b993620bcc38a66e8f6064
Harsha R Mundaragi <harsha.mundaragi@robosoftin.com>
2023-10-27 12:26:10 +0530

    create chuckUpPatient method in all the department. created the dictionary which holds all the data about chuckUp history.

--- MainFile.java
+++ MainFile.java
@@ -1,4 +1,11 @@
+import java.util.Dictionary;
+import java.util.HashMap;
+
 import AllFiles.*;
+
+enum Departments{
+    Orthopedics, Cardiology, Neurology, Pediatrics;
+}
 class MainFile{
     public static void main(String a[]){
         new Patient().patientCameToHospital();
